bbab4167b98a0928c8d412c4580d5a1b179e935b
Nolan Woods <nolan_w@sfu.ca>
2020-09-28 18:25:56 -0700

    Add Job

--- jobs/job.go
+++ jobs/job.go
@@ -1,10 +1,25 @@
 package jobs
 
-import "github.com/brinkmanlab/blend4go"
+import (
+	"context"
+	"github.com/brinkmanlab/blend4go"
+	"github.com/brinkmanlab/blend4go/histories"
+	"path"
+)
 
 type Job struct {
 	galaxyInstance *blend4go.GalaxyInstance
-	Id             blend4go.GalaxyID
+	Id             blend4go.GalaxyID `json:"id,omitempty"`
+	ToolId         string            `json:"tool_id,omitempty"`
+	UpdateTime     string            `json:"update_time,omitempty"`
+	HistoryId      string            `json:"history_id,omitempty"`
+	ExitCode       uint              `json:"exit_code,omitempty"`
+	State          string            `json:"state,omitempty"`
+	CreateTime     string            `json:"create_time,omitempty"`
+	ModelClass     string            `json:"model_class,omitempty"`
+	Inputs         interface{}       `json:"inputs,omitempty"`
+	Outputs        interface{}       `json:"outputs,omitempty"`
+	Params         interface{}       `json:"params,omitempty"`
 }
 
 func (j *Job) GetBasePath() string {
@@ -22,3 +37,40 @@ func (j *Job) GetID() blend4go.GalaxyID {
 func (j *Job) SetID(id blend4go.GalaxyID) {
 	j.Id = id
 }
+
+type invocationResponse struct {
+	Jobs                []Job                                           `json:"jobs"`
+	Outputs             []histories.HistoryDatasetAssociation           `json:"outputs"`
+	OutputCollections   []histories.HistoryDatasetCollectionAssociation `json:"output_collections"`
+	ImplicitCollections []histories.HistoryDatasetCollectionAssociation `json:"implicit_collections"`
+}
+
+func NewJob(ctx context.Context, g *blend4go.GalaxyInstance, payload map[string]interface{}) ([]Job, []histories.HistoryDatasetAssociation, []histories.HistoryDatasetCollectionAssociation, []histories.HistoryDatasetCollectionAssociation, error) {
+	//POST /api/tools Execute tool with a given parameter payload
+	if res, err := g.R(ctx).SetBody(payload).SetResult(&invocationResponse{}).Post("/api/tools"); err == nil {
+		r := res.Result().(invocationResponse)
+		return r.Jobs, r.Outputs, r.OutputCollections, r.ImplicitCollections, err
+	} else {
+		return nil, nil, nil, nil, err
+	}
+}
+
+func (j *Job) Delete(ctx context.Context) error {
+	return j.galaxyInstance.Delete(ctx, j)
+}
+
+func (j *Job) Resume(ctx context.Context) error {
+	_, err := j.galaxyInstance.R(ctx).Put(path.Join(j.GetBasePath(), j.GetID(), "resume"))
+	return err
+}
+
+//GET /api/jobs/{id}/common_problems
+//GET /api/jobs/{id}/inputs
+//GET /api/jobs/{id}/outputs
+//Delete /api/jobs/{id}
+//PUT /api/jobs/{id}/resume
+//GET /api/jobs/{job_id}/metrics
+//GET /api/jobs/{job_id}/destination_params
+//GET /api/jobs/{job_id}/parameters_display
+//GET /api/jobs/{id}/build_for_rerun
+//POST /api/jobs/{id}/error
--- jobs/jobs.go
+++ jobs/jobs.go
@@ -1,3 +1,30 @@
 package jobs
 
+import (
+	"context"
+	"github.com/brinkmanlab/blend4go"
+)
+
 const BasePath = "/api/jobs"
+
+func List(ctx context.Context, g *blend4go.GalaxyInstance) ([]Job, error) {
+	//GET /api/jobs
+	if res, err := g.List(ctx, BasePath, []Job{}, nil); err == nil {
+		return res.([]Job), nil
+	} else {
+		return nil, err
+	}
+}
+
+func Get(ctx context.Context, g *blend4go.GalaxyInstance, id blend4go.GalaxyID) (*Job, error) {
+	//GET /api/jobs/{id}
+	if res, err := g.Get(ctx, id, &Job{}); err == nil {
+		return res.(*Job), nil
+	} else {
+		return nil, err
+	}
+}
+
+//POST /api/jobs/search
+//GET /api/job_lock
+//PUT /api/job_lock
