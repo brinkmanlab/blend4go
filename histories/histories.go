7e73ac859c9a1cff1be1a576fcf4e496ce4e25fd
Nolan Woods <nolan_w@sfu.ca>
2020-10-08 14:48:24 -0700

    Fix history function return values

--- histories/histories.go
+++ histories/histories.go
@@ -8,38 +8,38 @@ import (
 
 const BasePath = "/api/histories"
 
-func list(ctx context.Context, g *blend4go.GalaxyInstance, category string) ([]History, error) {
+func list(ctx context.Context, g *blend4go.GalaxyInstance, category string) ([]*History, error) {
 	// GET /api/histories
 	// GET /api/histories/deleted return deleted histories for the current user
 	p := BasePath
 	if category != "" {
 		p = path.Join(BasePath, category)
 	}
-	if res, err := g.List(ctx, p, []History{}, nil); err == nil {
-		return res.([]History), nil
+	if res, err := g.List(ctx, p, []*History{}, nil); err == nil {
+		return res.([]*History), nil
 	} else {
 		return nil, err
 	}
 }
 
 // List histories for the current user
-func List(ctx context.Context, g *blend4go.GalaxyInstance) ([]History, error) {
+func List(ctx context.Context, g *blend4go.GalaxyInstance) ([]*History, error) {
 	return list(ctx, g, "")
 }
 
 // List deleted histories for the current user
-func ListDeleted(ctx context.Context, g *blend4go.GalaxyInstance) ([]History, error) {
+func ListDeleted(ctx context.Context, g *blend4go.GalaxyInstance) ([]*History, error) {
 	return list(ctx, g, "deleted")
 }
 
 // Get all histories that are published
-func GetPublished(ctx context.Context, g *blend4go.GalaxyInstance) ([]History, error) {
+func GetPublished(ctx context.Context, g *blend4go.GalaxyInstance) ([]*History, error) {
 	// GET /api/histories/published
 	return list(ctx, g, "published")
 }
 
 // Get all histories that are shared with the current user
-func GetSharedWithMe(ctx context.Context, g *blend4go.GalaxyInstance) ([]History, error) {
+func GetSharedWithMe(ctx context.Context, g *blend4go.GalaxyInstance) ([]*History, error) {
 	// GET /api/histories/shared_with_me
 	return list(ctx, g, "shared_with_me")
 }
