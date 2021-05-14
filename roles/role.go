6039844ded8eb5a024a2fb8039eca183326d22c6
Nolan Woods <nolan_w@sfu.ca>
2021-05-14 14:36:21 -0700

    Stub Role

--- roles/role.go
+++ roles/role.go
@@ -1,13 +1,40 @@
 package roles
 
-import "github.com/brinkmanlab/blend4go"
+import (
+	"context"
+	"github.com/brinkmanlab/blend4go"
+)
 
 type Role struct {
 	galaxyInstance *blend4go.GalaxyInstance
 	Id             string `json:"id,omitempty"`
 	Name           string `json:"name,omitempty"`
-	ModelClass     string `json:"model_class,omitempty"`
-	Url            string `json:"url,omitempty"`
 	Description    string `json:"description,omitempty"`
 	Type           string `json:"type,omitempty"`
+	Deleted        bool   `json:"-"`
+}
+
+func (r *Role) GetBasePath() string {
+	return BasePath
+}
+
+func (r *Role) SetGalaxyInstance(instance *blend4go.GalaxyInstance) {
+	r.galaxyInstance = instance
+}
+
+func (r *Role) GetID() blend4go.GalaxyID {
+	return r.Id
+}
+
+func (r *Role) SetID(id blend4go.GalaxyID) {
+	r.Id = id
+}
+
+func NewRole(ctx context.Context, name string) (*Role, error) {
+	// POST /api/roles Creates a new role.
+	panic("implement me")
+}
+
+func (r *Role) Update(ctx context.Context) error {
+	panic("implement me")
 }
--- roles/roles.go
+++ roles/roles.go
@@ -2,8 +2,51 @@
 // Relevant api endpoints are: `/api/roles`
 package roles
 
-// GET /api/roles Displays a collection (list) of roles.
+import (
+	"context"
+	"github.com/brinkmanlab/blend4go"
+)
 
-// GET /api/roles/{encoded_role_id} Displays information about a role.
+const BasePath = "/api/roles"
 
-// POST /api/roles Creates a new role.
+/*
+Displays a collection (list) of roles.
+deleted - If true, show deleted roles
+*/
+func List(ctx context.Context, g *blend4go.GalaxyInstance, deleted bool) ([]*Role, error) {
+	q := make(map[string]string)
+	if deleted {
+		q["deleted"] = "true"
+	}
+	// GET /api/roles GET /api/roles/deleted
+	var roles []*Role
+	_, err := g.List(ctx, BasePath, &roles, &q)
+	for _, role := range roles {
+		role.Deleted = deleted
+	}
+	return roles, err
+}
+
+// Displays information about a role.
+func Get(ctx context.Context, g *blend4go.GalaxyInstance, id blend4go.GalaxyID, deleted bool) (*Role, error) {
+	// GET /api/roles/{encoded_id} GET /api/roles/deleted/{encoded_id}
+	if res, err := g.Get(ctx, id, &Role{Deleted: deleted}, nil); err == nil {
+		return res.(*Role), err
+	} else {
+		return nil, err
+	}
+}
+
+// GetName get role by name
+func GetName(ctx context.Context, g *blend4go.GalaxyInstance, name string) (*Role, error) {
+	for _, deleted := range []bool{true, false} {
+		if roles, err := List(ctx, g, deleted); err == nil {
+			for _, role := range roles {
+				if role.Name == name {
+					return role, nil
+				}
+			}
+		}
+	}
+	return nil, nil
+}
