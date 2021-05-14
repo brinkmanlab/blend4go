93ef8ed59ff219fdea8b5e88c20b87da2178e1c8
Nolan Woods <nolan_w@sfu.ca>
2021-05-14 14:24:20 -0700

    Stub Group

--- groups/group.go
+++ groups/group.go
@@ -1,21 +1,104 @@
 package groups
 
-// GET /api/groups/{encoded_group_id} Displays information about a group.
+import (
+	"context"
+	"github.com/brinkmanlab/blend4go"
+	"github.com/brinkmanlab/blend4go/roles"
+	"github.com/brinkmanlab/blend4go/users"
+	"path"
+)
 
-// PUT /api/groups/{encoded_group_id} Modifies a group.
+type groupAssociations struct {
+	group       *Group
+	association string
+}
 
-// GET /api/groups/{encoded_group_id}/roles Displays a collection (list) of groups.
+func (ga *groupAssociations) Add(ctx context.Context, id blend4go.GalaxyID) error {
+	// PUT /api/groups/{encoded_group_id}/users/{encoded_user_id} Adds a user to a group
+	// PUT /api/groups/{encoded_group_id}/roles/{encoded_role_id} Adds a role to a group
+	panic("implement me")
+	if res, err := ga.group.galaxyInstance.R(ctx).Put(path.Join(BasePath, ga.group.Id, ga.association, id)); err == nil {
+		_, err := blend4go.HandleResponse(res)
+		return err
+	} else {
+		return err
+	}
+}
 
-// GET /api/groups/{encoded_group_id}/roles/{encoded_role_id} Displays information about a group role.
+func (ga *groupAssociations) Remove(ctx context.Context, id blend4go.GalaxyID) error {
+	// DELETE /api/groups/{encoded_group_id}/roles/{encoded_role_id} Removes a role from a group
+	// DELETE /api/groups/{encoded_group_id}/users/{encoded_user_id} Removes a user from a group
+	panic("implement me")
+	if res, err := ga.group.galaxyInstance.R(ctx).Delete(path.Join(BasePath, ga.group.Id, ga.association, id)); err == nil {
+		_, err := blend4go.HandleResponse(res)
+		return err
+	} else {
+		return err
+	}
+}
 
-// PUT /api/groups/{encoded_group_id}/roles/{encoded_role_id} Adds a role to a group
+type userGroupAssociations groupAssociations
 
-// DELETE /api/groups/{encoded_group_id}/roles/{encoded_role_id} Removes a role from a group
+func (u *userGroupAssociations) List(ctx context.Context) ([]*users.User, error) {
+	// GET /api/groups/{encoded_group_id}/users Displays a collection (list) of groups.
+	// GET /api/groups/{encoded_group_id}/users/{encoded_user_id} Displays information about a group user.
+	panic("implement me")
+}
 
-// GET /api/groups/{encoded_group_id}/users Displays a collection (list) of groups.
+type roleGroupAssociations groupAssociations
 
-// GET /api/groups/{encoded_group_id}/users/{encoded_user_id} Displays information about a group user.
+func (r *roleGroupAssociations) List(ctx context.Context) ([]*roles.Role, error) {
+	// GET /api/groups/{encoded_group_id}/roles Displays a collection (list) of roles.
+	// GET /api/groups/{encoded_group_id}/roles/{encoded_role_id} Displays information about a group role.
+	panic("implement me")
+}
 
-// PUT /api/groups/{encoded_group_id}/users/{encoded_user_id} Adds a user to a group
+type Group struct {
+	galaxyInstance *blend4go.GalaxyInstance
+	Id             blend4go.GalaxyID `json:"id,omitempty"`
+	Name           string            `json:"name,omitempty"`
+	Users          userGroupAssociations
+	Roles          roleGroupAssociations
+	Deleted        bool
+}
 
-// DELETE /api/groups/{encoded_group_id}/users/{encoded_user_id} Removes a user from a group
+func (g *Group) populateAssoc() {
+	g.Users.association = "users"
+	g.Roles.association = "roles"
+	g.Users.group = g
+	g.Roles.group = g
+}
+
+func NewGroup(ctx context.Context, name string) (*Group, error) {
+	g := &Group{
+		Name: name,
+	}
+	g.populateAssoc()
+	// POST /api/groups Creates a new group.
+	panic("implement me")
+}
+
+func (g *Group) GetBasePath() string {
+	return BasePath
+}
+
+func (g *Group) SetGalaxyInstance(instance *blend4go.GalaxyInstance) {
+	g.galaxyInstance = instance
+}
+
+func (g *Group) GetID() blend4go.GalaxyID {
+	return g.Id
+}
+
+func (g *Group) SetID(id blend4go.GalaxyID) {
+	g.Id = id
+}
+
+func (g *Group) Update(ctx context.Context) error {
+	// PUT /api/groups/{encoded_group_id} Modifies a group.
+	panic("implement me")
+}
+
+func (g *Group) Delete(ctx context.Context) error {
+	panic("implement me")
+}
--- groups/groups.go
+++ groups/groups.go
@@ -2,6 +2,63 @@
 // Relevant api endpoints are: `/api/groups`
 package groups
 
-// GET /api/groups Displays a collection (list) of groups.
+import (
+	"context"
+	"path"
 
-// POST /api/groups Creates a new group.
+	"github.com/brinkmanlab/blend4go"
+)
+
+const BasePath = "/api/groups"
+
+// get full group, populating model
+func get(ctx context.Context, g *blend4go.GalaxyInstance, model *Group) (*Group, error) {
+	// GET /api/groups/{encoded_id} GET /api/groups/deleted/{encoded_id}
+	if res, err := g.Get(ctx, model.GetID(), model, nil); err == nil {
+		group := res.(*Group)
+		group.populateAssoc()
+		return group, nil
+	} else {
+		return nil, err
+	}
+}
+
+// List groups
+// deleted - If true, show deleted groups
+func List(ctx context.Context, g *blend4go.GalaxyInstance, deleted bool) ([]*Group, error) {
+	fullpath := BasePath
+	if deleted {
+		fullpath = path.Join(BasePath, "deleted")
+	}
+	// GET /api/groups GET /api/groups/deleted
+	var groups []*Group
+	_, err := g.List(ctx, fullpath, &groups, nil)
+	for _, group := range groups {
+		group.Deleted = deleted
+		group, err = get(ctx, g, group)
+		if err != nil {
+			return nil, err
+		}
+		group.populateAssoc()
+	}
+	return groups, err
+}
+
+// Get group
+func Get(ctx context.Context, g *blend4go.GalaxyInstance, id blend4go.GalaxyID, deleted bool) (*Group, error) {
+	return get(ctx, g, &Group{Id: id, Deleted: deleted})
+}
+
+// GetName get group by name
+func GetName(ctx context.Context, g *blend4go.GalaxyInstance, name string) (*Group, error) {
+	for _, deleted := range []bool{true, false} {
+		if groups, err := List(ctx, g, deleted); err == nil {
+			for _, group := range groups {
+				if group.Name == name {
+					return group, nil
+				}
+			}
+		}
+	}
+	return nil, nil
+}
